a1e18d855a4465b680b4c311c51b620c54f8c764
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-10 16:05:59 +0200

    Adding readme File.

---
+++ src/test/java/com/rca/cics/CiCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rca.cics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
